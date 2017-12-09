8bce6461291a71e29f427174a61332f8b90df6ff
Maria Pietrzak <maria.pietrzak1@gmail.com>
2017-12-09 15:31:07 +0100

    Chair order section, functions improvement

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 //rozwijanie menu
 var li_firm = document.querySelectorAll(".menu_item");
-
 for(var i = 0; i < li_firm.length; i++){
     li_firm[i].addEventListener("click", function(event){
         var element = this.querySelector(".show");
@@ -8,6 +7,14 @@ for(var i = 0; i < li_firm.length; i++){
                 element.style.visibility = "visible";
             } else if (element.style.visibility == "visible") {
                 element.style.visibility = "hidden";
+            }
+    });
+}
+//chowanie menu
+for(var i = 0; i < li_firm.length; i++){
+    li_firm[i].querySelector('.show').addEventListener("mouseleave", function(event){
+            if(this.style.visibility == "visible"){
+                this.style.visibility = "hidden";
             }
     });
 }
@@ -67,6 +74,7 @@ button_pack[2].addEventListener("click", function(event){
 
 // rozwijanie list w panelu tworzącym zamówienie fotela
 var button_order = document.querySelectorAll('.list_arrow');
+var list = document.querySelectorAll('.list_panel li');
 var suma = 0;
 var suma_strong = document.querySelector('.sum strong');
 
@@ -80,70 +88,62 @@ function list_choose() {
         }
 }
 
-button_order[0].addEventListener('click', function(){
-    this.nextElementSibling.classList.remove('list_panel');
-    this.nextElementSibling.classList.add('list_panel_clicked');
-    var list = document.querySelectorAll('.list_panel_clicked li');
-
-    for(var i = 0; i < list.length; i++){
+for (var i = 0; i < button_order.length; i++){
+    button_order[i].addEventListener('click', function(){
+        this.nextElementSibling.classList.remove('list_panel');
+        this.nextElementSibling.classList.add('list_panel_clicked');
+        console.log(list)
+    list_choose();
+    });
+}
+for(var i = 0; i < list.length; i++){
+    console.log(i);
+    if( i == 0 || i == 1 || i == 2) {
         list[i].addEventListener('click', function(event){
             var text = this.innerHTML;
             var price = this.dataset.value;
             suma += parseInt(price,10);
-            suma_strong.innerHTML = suma;
             document.querySelector('.type').innerHTML = text;
             document.querySelector('.type_value').innerHTML = price;
+            suma_strong.innerHTML = suma;
+            price = 0;
+            suma = 0;
         });
-    }
-    list_choose();
-});
-button_order[1].addEventListener('click', function(){
-    this.nextElementSibling.classList.remove('list_panel');
-    this.nextElementSibling.classList.add('list_panel_clicked');
-    var list = document.querySelectorAll('.list_panel_clicked li');
-
-    for(var i = 0; i < list.length; i++){
+    } else if ( i == 3 || i == 4 || i == 5) {
         list[i].addEventListener('click', function(event){
             var text = this.innerHTML;
             var price = this.dataset.value;
             suma += parseInt(price,10);
-            suma_strong.innerHTML = suma;
             document.querySelector('.color').innerHTML = text;
             document.querySelector('.color_value').innerHTML = price;
+            suma_strong.innerHTML = suma;
+            price = 0;
+            suma = 0;
         });
-    }
-    list_choose();
-});
-
-button_order[2].addEventListener('click', function(){
-    this.nextElementSibling.classList.remove('list_panel');
-    this.nextElementSibling.classList.add('list_panel_clicked');
-    var list = document.querySelectorAll('.list_panel_clicked li');
-
-    for(var i = 0; i < list.length; i++){
+    } else if ( i == 6 || i == 7 || i == 8) {
         list[i].addEventListener('click', function(event){
             var text = this.innerHTML;
             var price = this.dataset.value;
             suma += parseInt(price,10);
-            suma_strong.innerHTML = suma;
             document.querySelector('.pattern').innerHTML = text;
             document.querySelector('.pattern_value').innerHTML = price;
-        });
+            suma_strong.innerHTML = suma;
+            price = 0;
+            suma = 0;
+        })
     }
-    list_choose();
-});
+}
 
 //zamówienie fotela
 var order_button = document.querySelector('.order_button');
 order_button.addEventListener('click', function(){
     var left = document.querySelectorAll('.panel_left span');
     var right = document.querySelectorAll('.panel_right span');
-
     function remove(element, index, array){
         element.innerHTML = "";
     }
-
     left.forEach(remove);
     right.forEach(remove);
     suma_strong.innerHTML = "";
+    suma = 0;
 });
